933277af825d521d4630254a3cff73e6dae6177c
galperins4 <galperins4@gmail.com>
2018-06-16 23:42:05 -0500

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -76,8 +76,19 @@ if __name__ == '__main__':
                        "senderPublicKey":"030dee498e6ff341ec3a51df821d5d39d5ac19142ac27eba9fd1f13b6e30dc1528",
                        "signature":"304402203b39b925179858ac24ce58d10ff4ca70f756a0cf48db9e69638d08eb87b977f6022015dcacbace1ed2e13d0be497147585300467ce83f57d577e67755395666e7c85",
                        "id":"ca8dcc428804b745422915f5e6ab41020b951c56e331ac3c397932255e474622"}
-
+            
+            test_tx_two = {"type":0,
+                           "amount":100100000000,
+                           "fee":10000000,
+                           "recipientId":"DS2YQzkSCW1wbTjbfFGVPzmgUe1tNFQstN",
+                           "timestamp":39109195,"asset":{},
+                           "vendorField":"core2test",
+                           "senderPublicKey":"030dee498e6ff341ec3a51df821d5d39d5ac19142ac27eba9fd1f13b6e30dc1528",
+                           "signature":"3044022016976a0cd4fe93559311b11b743ac0baad650209c98d407068ff0fa24cd25ae502200577bddbd111ffd0ad5033d80326efc45fb51ee2b6a77fd18d17c578169380bd",
+                           "id":"8797c8677c928aa074af04c43569f2355919bd034c6bc8347b6a6a856f8bdae2"}
+            
             signed_tx.append(test_tx)
+            signed_tx.append(test_tx_two)
             
             broadcast(signed_tx, ark)
             snekdb.processStagedPayment(unique_rowid)
