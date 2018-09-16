45fad57fec52cbcef5fb9a8a2c51ddae0616c8ca
galperins4 <galperins4@gmail.com>
2018-09-16 15:15:52 -0500

    Update sql.py

--- core/util/sql.py
+++ core/util/sql.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class SnekDB:
     def __init__(self, u):
-        self.connection=sqlite3.connect('/home/'+u+'/core2_tbw/ark.db')
+        self.connection=sqlite3.connect('/home/'+u+'/core2_tbw/ark.db', check_same_thread=False)
         self.cursor=self.connection.cursor()
 
 
