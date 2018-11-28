71a99ec59f6c1a6104690a87b5d6502184d8d51e
galperins4 <galperins4@gmail.com>
2018-11-27 19:46:03 -0600

    Update ark.py

--- core/util/ark.py
+++ core/util/ark.py
@@ -13,12 +13,13 @@ class ArkDB:
         self.PublicKey = pk
         self.cursor=self.connection.cursor()
 
-    def blocks(self, i='no', val=1):
+    def blocks(self, i='no', val=1, h=None):
         
         #if i is yes, first run grab every block forged for history
         if i == 'yes':
             try:
-                self.cursor.execute(f"""SELECT "id","timestamp","reward","total_fee","height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' ORDER BY "height" DESC""")
+                self.cursor.execute(f"""SELECT "id","timestamp","reward","total_fee",
+                "height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' ORDER BY "height" DESC""")
                 return self.cursor.fetchall()
             except Exception as e:
                 print(e)
@@ -26,16 +27,17 @@ class ArkDB:
         # interval check to audit webhook for missing blocks
         elif i == 'interval':
             try:
-                self.cursor.execute(
-                    f"""SELECT "id","timestamp","reward","total_fee","height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' ORDER BY "height" DESC LIMIT {val}""")
+                self.cursor.execute(f"""SELECT "id","timestamp","reward","total_fee",
+                "height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' ORDER BY "height" DESC LIMIT {val}""")
                 return self.cursor.fetchall()
             except Exception as e:
                 print(e)
 
-        #else just grab last 50 for normal processing
+        #else just grab last x for normal processing
         else:
             try:
-                self.cursor.execute(f"""SELECT "id","timestamp","reward","total_fee","height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' ORDER BY "height" DESC LIMIT 50""")
+                self.cursor.execute(f"""SELECT "id","timestamp","reward","total_fee",
+                "height" FROM blocks WHERE "generator_public_key" = '{self.PublicKey}' and "height" > {h} ORDER BY "height" DESC LIMIT 50""")
                 return self.cursor.fetchall()
             except Exception as e:
                 print(e)   
