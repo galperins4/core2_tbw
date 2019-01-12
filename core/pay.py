f1bc7273e243aa635779e25fae81bd83f1d7201a
galperins4 <galperins4@gmail.com>
2019-01-11 21:13:42 -0600

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -15,7 +15,7 @@ atomic = 100000000
 
 def get_client(ip="localhost", api_version='v2'):
     port = network[data['network']]['port']
-    return ArkClient('http://{0}:{1}/api'.format(ip, port), api_version=api_version)
+    return ArkClient('http://{0}:{1}/api'.format(ip, port))
 
 
 def broadcast(tx):
