cc16ff0b68faef096693b178c217a57ce498acbc
galperins4 <galperins4@gmail.com>
2019-01-15 22:38:36 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -70,20 +70,27 @@ def allocate(lb):
         # filter out 0 balances for processing
         if bal > 0:
             share_weight = bal / approval  # calc share rate
-
-            # get custom share rate if applicable
-            custom_share = snekdb.getVoterShare(i[0]).fetchall()
-            cshare = block_reward * custom_share[0][0]
-
-            # get the difference between normal share and custom share
-            if custom_share[0][0] == data['voter_share']:
-                reward = int(share_weight * vshare)
-                remainder_reward = 0
-            else:
+            # fixed processing
+            if i[0] in data['fixed'].keys():
+                fixed_amt = data['fixed'][i[0]] * atomic
+                reward = int(fixed_amt/data['interval'])
                 treward = int(share_weight * vshare)
-                reward = int(share_weight * cshare)
                 remainder_reward = int(treward - reward)
                 delegate_check += remainder_reward
+            else:
+                # get custom share rate if applicable
+                custom_share = snekdb.getVoterShare(i[0]).fetchall()
+                cshare = block_reward * custom_share[0][0]
+
+                # get the difference between normal share and custom share
+                if custom_share[0][0] == data['voter_share']:
+                    reward = int(share_weight * vshare)
+                    remainder_reward = 0
+                else:
+                    treward = int(share_weight * vshare)
+                    reward = int(share_weight * cshare)
+                    remainder_reward = int(treward - reward)
+                    delegate_check += remainder_reward
 
             # update reserve from blacklist assign
             if i[0] == data["blacklist_assign"]:
