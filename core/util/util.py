d7e24112989c5d6c787cc9fba70356ce8d5abc5f
galperins4 <galperins4@gmail.com>
2019-01-29 20:22:16 -0600

    Update util.py

--- core/util/util.py
+++ core/util/util.py
@@ -1,13 +1,26 @@
 import json
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 class Util():
-    def __init__(self):
+    def __init__(self, coin network):
+        self.network = network
+        self.coin = coin
         self.home = str(Path.home())
-        self.config_path = ''
-   
+        self.core_config = '/.config/'+coin+"-core/"+network
+        self.tbw_config = '/core2_tbw/config'
+        
+        self.core, self.tbw = get_paths()
+
+
+    def get_paths(self):
+        core_config_path = self.home+self.core_config
+        tbw_config_path = self.home+self.tbw_config
+        
+        return core_config_path, tbw_config_path
+        
     
     def get_client(self):
         pass
