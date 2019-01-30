db2888d5801ea26dd550fd9c28b6631de0099982
galperins4 <galperins4@gmail.com>
2019-01-29 20:08:09 -0600

    Update util.py

--- core/util/util.py
+++ core/util/util.py
@@ -7,11 +7,15 @@ class Util():
     def __init__(self):
         self.home = str(Path.home())
         self.config_path = ''
- 
-
-    def get_configs(self):
-        pass
-    
+   
     
     def get_client(self):
+        pass
+
+
+    def parse_configs(self):
+        pass    
+    
+    
+    def parse_pool(self):
         pass
