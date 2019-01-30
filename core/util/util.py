79e5bd77c00ba424d4987e923fe3d163caed4af5
galperins4 <galperins4@gmail.com>
2019-01-29 20:06:44 -0600

    Update util.py

--- core/util/util.py
+++ core/util/util.py
@@ -1,1 +1,17 @@
-# utility file
+import json
+import os
+from pathlib import Path
+
+
+class Util():
+    def __init__(self):
+        self.home = str(Path.home())
+        self.config_path = ''
+ 
+
+    def get_configs(self):
+        pass
+    
+    
+    def get_client(self):
+        pass
