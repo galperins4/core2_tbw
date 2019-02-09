9116730b6caadd4848e29b5ad3b794358492091a
galperins4 <galperins4@gmail.com>
2019-02-08 22:05:59 -0600

    Update util.py

--- core/util/util.py
+++ core/util/util.py
@@ -4,35 +4,16 @@ from pathlib import Path
 
 
 class Util():
-    def __init__(self):
+    def __init__(self, n):
         self.home = str(Path.home())
         self.tbw = self.home+'/core2_tbw/config'
         
         self.data = self.parse_configs()
-        net = self.data['network'].split('_')
+        net = n.split('_')
         coin, network = net[0], net[1]
     
         self.core = self.home+'/.config/'+coin+'-core/'+network
         
         
     def get_client(self, api_port, ip="localhost"):
-        #port = self.network[self.data['network']]['port']
         return ArkClient('http://{0}:{1}/api'.format(ip, api_port))
-
-
-    def parse_configs(self):
-        with open((self.tbw+'/config.json')) as data_file:
-            d = json.load(data_file)
-        #with open((self.tbw+'/networks.json')) as network_file:
-        #    n = json.load(network_file)
-
-        return d    
-    
-    
-    def parse_pool(self):
-        with open((self.tbw+'/pool.json')) as data_file:
-            d = json.load(data_file)
-        #with open((self.tbw+'/networks.json')) as network_file:
-        #    n = json.load(network_file)
-        
-        return d
