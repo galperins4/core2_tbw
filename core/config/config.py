811ee665206149ae81483af78ed2a1a7f7012f1c
galperins4 <galperins4@gmail.com>
2019-02-08 21:25:54 -0600

    Update config.py

--- core/config/config.py
+++ core/config/config.py
@@ -13,29 +13,29 @@ class Config():
     
     
     def load_tbw_config(self):
-        self.start_block
-        self.network
-        self.delegate_ip
-        self.database_user
-        self.public_key
-        self.interval
-        self.voter_share
-        self.passphrase
-        self.secondphrase
-        self.voter_msg
-        self.block_check
-        self.cover_tx_fee
-        self.vote_cap
-        self.vote_min
-        self.fixed
-        self.whitelist
-        self.whitelist_addr
-        self.blacklist
-        self.blacklist_addr
-        self.blacklist_assign
-        self.min_payment
-        self.keep
-        self.pay_addresses
+        self.start_block = os.getenv("START_BLOCK")
+        self.network = os.getenv("NETWORK")
+        self.delegate_ip = os.getenv("DELEGATE_IP")
+        self.database_user = os.getenv("DATABASE_USER")
+        self.public_key = os.getenv("PUBLIC_KEY")
+        self.interval = os.getenv("INTERVAL")
+        self.voter_share = os.getenv("VOTER_SHARE")
+        self.passphrase = os.getenv("PASSPHRASE")
+        self.secondphrase = os.getenv("SECONDPHRASE")
+        self.voter_msg = os.getenv("VOTER_MSG")
+        self.block_check = os.getenv("BLOCK_CHECK")
+        self.cover_tx_fee = os.getenv("COVER_TX_FEE")
+        self.vote_cap = os.getenv("VOTE_CAP")
+        self.vote_min = os.getenv("VOTE_MIN")
+        self.fixed = os.getenv("FIXED").split(',')
+        self.whitelist = os.getenv("WHITELIST")
+        self.whitelist_addr = os.getenv("WHITELIST_ADDR").split(',')
+        self.blacklist = os.getenv("BLACKLIST")
+        self.blacklist_addr = os.getenv("BLACKLIST_ADDR").split(',')
+        self.blacklist_assign = os.getenv("BLACKLIST_ASSIGN")
+        self.min_payment = os.getenv("MIN_PAYMENT")
+        self.keep = os.getenv("KEEP").split(',')
+        self.pay_addresses = os.getenv("PAY_ADDRESSES").split(',')
         
         
     def load_pool_config(self):
@@ -47,7 +47,3 @@ class Config():
         self.pool_port = os.getenv("POOL_PORT")
         self.custom_port = os.getenv("CUSTOM_PORT")
         self.pool_version = os.getenv("POOL_VERSION")
-        
-                
-        self.epoch = os.getenv("EPOCH").split(',')
-
