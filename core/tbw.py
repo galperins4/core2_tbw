9de29042cf0e74414a4155f78f56b2d8df13ecc8
galperins4 <galperins4@gmail.com>
2019-02-23 22:53:34 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -415,8 +415,10 @@ if __name__ == '__main__':
     data, network = u.parse_configs()
 
     dynamic = Dynamic(data['dbusername'], data['voter_msg'])
-    dynamic.get_node_configs()
+    #dynamic.get_node_configs()
     transaction_fee = dynamic.get_dynamic_fee()
+    print(transaction_fee)
+    quit()
     
     # initialize db connection
     # get database
