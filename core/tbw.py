949bf19acdaf7f819d5bbc3dbe13c80ee610263a
galperins4 <galperins4@gmail.com>
2019-03-11 22:23:04 -0500

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -201,6 +201,8 @@ def get_voters():
     # get voters
     #initial_voters = arkdb.voters()
     v = client.delegates.voter_balances(delegate_id=data["delegate"])
+    print(v['data'])
+    quit()
     initial_voters = [(i,v[i]) for i in v['data']]
     
     
