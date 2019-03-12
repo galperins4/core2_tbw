db4caeaf7519e1b4b931a0a673f1265d316da443
galperins4 <galperins4@gmail.com>
2019-03-11 21:59:09 -0500

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -21,12 +21,9 @@ def broadcast(tx):
         records = [[j['recipientId'], j['amount'], j['id']] for j in tx]
         time.sleep(1)
     except BaseException:
-        # fall back to delegate node to grab data needed
-        backup_client = get_client(data['delegate_ip'])
-        transaction = backup_client.transactions.create(tx)
-        print(transaction)
-        records = [[j['recipientId'], j['amount'], j['id']] for j in tx]
-        time.sleep(1)
+        # error
+        print("Something went wrong")
+        quit()
 
     snekdb.storeTransactions(records)
     
