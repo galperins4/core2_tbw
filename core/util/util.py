8bd72cab1d18c4afce7af6ddfe800f624c23e73d
galperins4 <galperins4@gmail.com>
2019-04-02 21:10:13 -0500

    Update util.py

--- core/util/util.py
+++ core/util/util.py
@@ -1,38 +1,15 @@
-import json
 from client import ArkClient
 from pathlib import Path
 
 
-class Util():
-    def __init__(self):
+class Util:
+    def __init__(self, n):
         self.home = str(Path.home())
-        self.tbw = self.home+'/core2_tbw'
-        
-        self.data, self.network = self.parse_configs()
-        net = self.data['network'].split('_')
+        net = n.split('_')
         coin, network = net[0], net[1]
-       
+    
         self.core = self.home+'/.config/'+coin+'-core/'+network
         
-
-    def get_client(self, ip="localhost"):
-        port = self.network[self.data['network']]['port']
-        return ArkClient('http://{0}:{1}/api'.format(ip, port))
-
-
-    def parse_configs(self):
-        with open((self.tbw+'/config/config.json')) as data_file:
-            d = json.load(data_file)
-        with open((self.tbw+'/config/networks.json')) as network_file:
-            n = json.load(network_file)
-
-        return d, n    
-    
-    
-    def parse_pool(self):
-        with open((self.tbw+'/config/pool.json')) as data_file:
-            d = json.load(data_file)
-        with open((self.tbw+'/config/networks.json')) as network_file:
-            n = json.load(network_file)
         
-        return d, n
+    def get_client(self, api_port, ip="localhost"):
+        return ArkClient('http://{0}:{1}/api'.format(ip, api_port))
