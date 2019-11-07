d7f0ee35a61a14557a28e4d81790883afdacc66d
galperins4 <galperins4@gmail.com>
2019-11-06 21:25:40 -0600

    Create write.py

---
+++ core/util/write.py
@@ -0,0 +1,15 @@
+import os
+import json
+from Naked.toolshed.shell import muterun_js
+
+class JsWrite():
+    
+    def write():
+        pass
+    
+    def read():
+        pass
+        
+        
+    def delete():
+        pass
