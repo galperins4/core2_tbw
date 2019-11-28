6a099679b18c341c5bddac2d69def857e375335f
galperins4 <galperins4@gmail.com>
2019-11-27 22:23:25 -0600

    Update dynamic.py

--- core/util/dynamic.py
+++ core/util/dynamic.py
@@ -21,7 +21,7 @@ class Dynamic:
     
     def get_multipay_limit(self):
         client = self.u.get_client(self.port)
-        node_configs = client.node.configuration()['data']['"multiPaymentLimit']
+        node_configs = client.node.configuration()['data']['constants']['"multiPaymentLimit']
         print(node_configs)
         quit()
     
