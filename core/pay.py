f980e5ff11d254a65b67391124e34db1c271805f
galperins4 <galperins4@gmail.com>
2019-12-11 22:13:20 -0600

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -92,10 +92,26 @@ def share_multipay():
         max_tx = dynamic.get_multipay_limit()
         unprocessed_pay = snekdb.stagedArkPayment(int(max_tx)).fetchall()
         
-        print(len(unprocessed_pay))
+        if len(unprocessed_pay) == 1:
+            unique_rowid = [y[0] for y in unprocessed_pay]
+            check = {}
+
+            temp_nonce = get_nonce()+1
+            for i in unprocessed_pay:
+                transaction_fee = dynamic.get_dynamic_fee()
+
+                # fixed processing
+                if i[1] in data.fixed.keys():
+                    fixed_amt = int(data.fixed[i[1]] * data.atomic)
+                    tx = build_transfer_transaction(i[1], (fixed_amt), i[3], transaction_fee, data.passphrase, data.secondphrase, str(temp_nonce))
+                else:           
+                    tx = build_transfer_transaction(i[1], (i[2]), i[3], transaction_fee, data.passphrase, data.secondphrase, str(temp_nonce))
+                check[tx['id']] = i[0]
+                signed_tx.append(tx)
+                broadcast(signed_tx)
 
         # query not empty means unprocessed blocks
-        if unprocessed_pay:
+        elif unprocessed_pay:
             unique_rowid = [y[0] for y in unprocessed_pay]
             check = {}
             nonce = int(get_nonce() + 1)
