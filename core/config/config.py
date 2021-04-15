1af54eba46e51a135a6d42c20783082b0482f5e0
galperins4 <galperins4@gmail.com>
2021-04-14 19:01:05 -0500

    Update config.py

--- core/config/config.py
+++ core/config/config.py
@@ -10,6 +10,7 @@ class Config():
         self.atomic = int(os.getenv("ATOMIC"))
         self.load_tbw_config()
         self.load_pool_config()
+        self.load_experimental_config()
         self.format_fixed()
         self.format_keep()
         self.format_payaddr()
@@ -39,7 +40,15 @@ class Config():
         self.pay_addresses = os.getenv("PAY_ADDRESSES").split(',')
         self.multi = os.getenv("MULTI")
         
-        
+    
+    def load_experimental_config(self):
+        self.exchange = os.getenv("EXCHANGE")
+        self.convert_from = os.getenv("CONVERT_FROM")
+        self.convert_to = os.getenv("CONVERT_TO")
+        self.address_to = os.getenv("ADDRESS_TO")
+        self.network_to = os.getenv("NETWORK_TO")
+    
+    
     def load_pool_config(self):
         self.pool_ip = os.getenv("POOL_IP")
         self.explorer = os.getenv("EXPLORER")
