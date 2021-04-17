86479d93fca97250b64f4250bd5ba870aaf6ae3e
galperins4 <galperins4@gmail.com>
2021-04-17 16:16:52 -0500

    Update exchange.py

--- core/util/exchange.py
+++ core/util/exchange.py
@@ -1,5 +1,7 @@
+import json
 import math
 import requests
+from lowercase_booleans import false
 
 
 class Exchange:
@@ -25,7 +27,35 @@ class Exchange:
         return pay
     
     def process_simpleswap_exchange(self, address, amount):
-        pass  
+        fixed = false
+        print("Processing Exchange")
+        print("Original Amount", amount)
+        amount = self.truncate((amount / self.atomic),4)
+        print("Exchange Amount:", amount)
+        url = 'https://t1mi6dwix2.execute-api.us-west-2.amazonaws.com/Test/exchange'
+        data_in = {"fixed": fixed,
+                   "currency_from": self.config.convert_from,
+                   "currency_to": self.config.convert_to,
+                   "address_to": self.config.address_to,
+                   "amount": str(amount),
+                   "user_refund_address":address}
+        
+        res_bytes={}
+        res_bytes['data'] = json.dumps(data_in).encode('utf-8')
+        
+        try:
+            r = requests.get(url, params=res_bytes)
+            if r.json()['status'] == "success":
+                payin_address = r.json()['payinAddress']
+                exchangeid = r.json()['exchangeId']
+                self.database.storeExchange(address, payin_address, self.config.address_to, amount, exchangeid)
+                print("Exchange Success")   
+        except:
+            payin_address = address
+            print("Exchange Fail")
+    
+        print("Pay In Address", payin_address)
+        return payin_address
     
     
     def process_changenow_exchange(self, address, amount):
