a48ecae3c2614cadaa19b01085f72661d0d2b0ae
galperins4 <galperins4@gmail.com>
2022-04-19 19:31:14 -0500

    Update dynamic.py

--- core/util/dynamic.py
+++ core/util/dynamic.py
@@ -47,16 +47,10 @@ class Dynamic:
                 second_sig = 64
                 per_tx_fee = 29
                 v_msg = len(self.msg) 
-                print("vendor length", v_msg)
                 tx_size = multi_tx + v_msg + second_sig + (numtx * per_tx_fee)
-                print("dynamic offset", dynamic_offset)
-                print("transaction size", tx_size)
-                print("fee multiplier", fee_multiplier)
-                
+
                 # calculate transaction fee
                 transaction_fee = self.calculate_dynamic_multifee(dynamic_offset, tx_size, fee_multiplier)
-                #transaction_fee = self.calculate_dynamic_fee(dynamic_offset, tx_size, fee_multiplier)
-                print("calculated transaction fee", transaction_fee)
          
         except:
             transaction_fee = int(0.1 * atomic)
