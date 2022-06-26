a1fed9bd81e1f4b9f7f8bcbf7c7f8dea4d11750e
galperins4 <galperins4@gmail.com>
2022-06-26 18:56:47 -0500

    Update dynamic.py

--- core/util/dynamic.py
+++ core/util/dynamic.py
@@ -14,15 +14,10 @@ class Dynamic:
         self.u = Util(self.network)
         self.client = self.u.get_client(self.port)
         
-    '''
-    def calculate_dynamic_fee(self, t, s, c):
-        fee = int((t + s) * c)
-        return fee
-    '''
     
     def get_multipay_limit(self):
         try:
-            limit = int(self.client.node.configuration()['data']['constants']['multiPaymentLimit'])
+            limit = int(self.client.node.configuration()['data']['constants']['transfer']['maximum'])
         except:
             limit = 20
         return limit
@@ -35,11 +30,11 @@ class Dynamic:
     
     def get_dynamic_fee(self, numtx):
         try:
-            node_configs = self.client.node.configuration()['data']['transactionPool']['dynamicFees']
+            node_configs = self.client.node.configuration()['data']['pool']['dynamicFees']
             if (node_configs['enabled'] == "False"):
                 transaction_fee = int(0.1 * atomic)
             else:
-                dynamic_offset = node_configs['addonBytes']['multiPayment']
+                dynamic_offset = node_configs['addonBytes']['transfer']
                 fee_multiplier = node_configs['minFeePool']
                 
                 # get size of transaction
@@ -56,26 +51,3 @@ class Dynamic:
             transaction_fee = int(0.1 * atomic)
             
         return transaction_fee
-    
-    '''
-    def get_dynamic_fee(self):        
-        try:
-            node_configs = self.client.node.configuration()['data']['transactionPool']['dynamicFees']
-            if (node_configs['enabled'] == "False"):
-                transaction_fee = int(0.1 * atomic)
-            else:
-                dynamic_offset = node_configs['addonBytes']['transfer']
-                fee_multiplier = node_configs['minFeePool']
-                
-                # get size of transaction - S
-                standard_tx = 230
-                v_msg = len(self.msg) 
-                tx_size = standard_tx + v_msg
-                
-                # calculate transaction fee
-                transaction_fee = self.calculate_dynamic_fee(dynamic_offset, tx_size, fee_multiplier)
-        except:
-            transaction_fee = int(0.1 * atomic)
-
-        return transaction_fee
-    '''
